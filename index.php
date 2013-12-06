3fd789d74537a97eb81566fbe02fa12d6f632979
Javier <jlalovi@gmail.com>
2013-12-06 14:31:27 +0100

    DB credentials included

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn_string = "";
+$conn_string = "dbname=d9dh4a3vip6rqq host=ec2-184-72-238-68.compute-1.amazonaws.com port=5432 user=xvizqzyxlamykm password=UESatB7lmlJwgWxW7hDPm4YXnn sslmode=require";
 
 $dbconn = pg_connect($conn_string) 
           or die('Could not connect: ' . pg_last_error());
